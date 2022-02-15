24fa4fcf637187b8e1db8bcd593a639f9cbd15f9
Oleg Novikov <novikov7@yahoo.com>
2022-02-15 11:52:15 +0300

    first commit

--- app/src/main/java/com/example/testproject/MainActivity.java
+++ app/src/main/java/com/example/testproject/MainActivity.java
@@ -2,15 +2,30 @@ package com.example.testproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.ChartData;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.Chart;
+import lecho.lib.hellocharts.view.LineChartView;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     Spinner spinner;
@@ -18,6 +33,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
    private EditText user_field;
    private Button button;
    private TextView result;
+   private ImageView photo;
+   private LineChartView chart;
+    private Object List;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,16 +48,48 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         spinner.setOnItemSelectedListener(this);
         user_field = findViewById(R.id.user_field);
         button = findViewById(R.id.button);
-        result = findViewById(R.id.result);
+        photo = findViewById(R.id.photo);
+        chart = findViewById(R.id.chart);
 
+
+        setButtonListener();
+        setUpChart();
+    }
+
+    public void setButtonListener() {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String spinnerText = spinner.getSelectedItem().toString();
-        Toast.makeText(MainActivity.this, user_field.getText() + spinnerText, Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, user_field.getText() + spinnerText, Toast.LENGTH_LONG).show();
+                photo.setVisibility(View.INVISIBLE);
+
             }
         });
     }
+
+    public void setUpChart(){
+        Axis x = new Axis();
+        ChartData a = chart.getChartData();
+        a.setAxisXBottom(x);
+        a.setAxisYLeft(x);
+        List<PointValue> values = new ArrayList<PointValue>();
+        values.add(new PointValue(0, 3));
+        values.add(new PointValue(2, 2));
+        values.add(new PointValue(2, 2));
+        values.add(new PointValue(3, 3));
+        ChartData chartData = chart.getChartData();
+        LineChartData lineChartData = chart.getLineChartData();
+
+
+        Line line = new Line(values).setColor(Color.GREEN).setCubic(true);
+        List<Line> lines = new ArrayList<Line>();
+        lines.add(line);
+
+        lineChartData.setLines(lines);
+
+    }
+
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
@@ -51,4 +101,5 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
-}
+
+    }
